09eca236276e3da6c53f7c8e400cc9ec82b174f0
Viet Nguyen Hoang <vietgaming1312004@gmail.com>
2024-06-11 03:10:55 +0700

    Delete back-end directory

--- back-end/db/db-creation.sql
+++
@@ -1,6 +0,0 @@
-CREATE DATABASE "Viet-PERN-ecom"
-  WITH
-  OWNER = postgres
-  ENCODING = 'UTF8'
-  CONNECTION LIMIT = -1
-  IS_TEMPLATE = False;
--- back-end/routes/middleware.js
+++
@@ -1,8 +0,0 @@
-const requireLogin = (req, res, next) => {
-  if (!req.isAuthenticated()) {
-    return res.status(401).send('You must be logged in to access this endpoint.');
-  }
-  next();
-};
-
-module.exports = requireLogin;
